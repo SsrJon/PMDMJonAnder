120620844fe86e1a70e661d19137d5ec98bd7e98
SsrJon <jonanderdecastro@gmail.com>
2020-01-20 15:50:17 +0100

    Mapa

--- app/src/main/java/com/example/pmdmjonander/MainActivity.java
+++ app/src/main/java/com/example/pmdmjonander/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.pmdmjonander;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnMapa, btnMarcador;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMapa=findViewById(R.id.buttonMapa);
+        btnMarcador=findViewById(R.id.buttonMarcador);
+
+
+
+        btnMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent mapa = new Intent(MainActivity.this, Mapa.class);
+                startActivity(mapa);
+
+            }
+        });
+
+
+        btnMarcador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent marcador = new Intent(MainActivity.this, Marcador.class);
+                startActivity(marcador);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/pmdmjonander/Mapa.java
+++ app/src/main/java/com/example/pmdmjonander/Mapa.java
@@ -2,13 +2,167 @@ package com.example.pmdmjonander;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Icon;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import java.util.List;
 
-public class Mapa extends AppCompatActivity {
+
+public class Mapa extends AppCompatActivity implements
+        OnMapReadyCallback, PermissionsListener {
+
+
+    private PermissionsManager permissionsManager;
+    MarkerOptions marcador = new MarkerOptions();
+    private MapboxMap mapboxMap;
+    private MapView mapView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
         setContentView(R.layout.activity_mapa);
+
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+
+        Mapa.this.mapboxMap = mapboxMap;
+
+        //Se añade el marcador
+        marcador.title("punto");
+        IconFactory iconFactoryMarcador = IconFactory.getInstance(Mapa.this);
+        Icon iconMarcador = iconFactoryMarcador.fromResource(R.drawable.marker);
+        marcador.icon(iconMarcador);
+        marcador.position(new LatLng(43.257306, -2.903710));
+        mapboxMap.addMarker(marcador);
+
+
+        mapboxMap.setOnMarkerClickListener(new MapboxMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(@NonNull Marker marker) {
+
+
+                Intent marcador = new Intent(Mapa.this, Marcador.class);
+                startActivity(marcador);
+
+                return false;
+            }
+        });
+
+
+        //Style del mapa
+        mapboxMap.setStyle(Style.OUTDOORS, new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+
+                //Ubicación actual
+                enableLocationComponent(style);
+
+
+            }
+        });
+
+
+
+
+    }
+
+
+    //Ubicacion actual del dispositivo
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+
+
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            LocationComponent locationComponent = mapboxMap.getLocationComponent();
+            locationComponent.activateLocationComponent(LocationComponentActivationOptions.builder(this, loadedMapStyle).build());
+            locationComponent.setLocationComponentEnabled(true);
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+
+        }
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+
     }
 }
